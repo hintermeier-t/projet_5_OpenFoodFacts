1413e7191b6a65d993b4ce19faa0585223c5c856
Thomas Hintermeier <hintermeier-t@protonmail.com>
2020-04-28 15:09:48 +0200

    Premier commit, vide

---
+++ install.py
@@ -0,0 +1,10 @@
+"""
+    Installation module
+    ===================
+
+    This module will be called only if the "install" option is mentionned when calling "main.py"
+
+    :Example:
+    $ pipenv run python -m main.py install
+
+"""    
---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+import peewee
+
+if 'install' in str(sys.argv):
+    #Call installation script
+    print("Hello World")
+
