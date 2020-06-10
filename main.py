9bf11e48f076daaff11bd7f2f948cb84ca68066a
Thomas Hintermeier <hintermeier-t@protonmail.com>
2020-06-10 14:07:42 +0200

    Harmonisation du code (Première passe rapide)

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-import app
-import tables
-import sys
-import install
+# coding: utf-8
 
+#- Importing Standard Modules:
 import sys
-
 import getpass #  Will be used to enter the user password when accessing / creating Database
 
-import tables.py
+#- Importing Custom Modules:
+import app
+import tables
+import install
 
 def main ():
+    db = tables.db
+    db.close() #- if not closed properly on precedent use
 
+    if "install" in sys.argv: 
+        
+        db.connect()
+        raw_data = install.Data()
+        db.create_tables([tables.Products, tables.Categories, tables.Stores, tables.Categorized, tables.Buyable, tables.Substitutes])
+        clean_data = install.Cleaner(raw_data.data)
+        save = install.Saver(clean_data.cleaned_data)
+        db.close()
 
-db = tables.db
-
-db.close() #- if not closed properly on precedent use
-
-if "install" in sys.argv: 
-    
     db.connect()
-    raw_data = install.Data()
-    db.create_tables([tables.Products, tables.Categories, tables.Stores, tables.Categorized, tables.Buyable, tables.Substitutes])
-    clean_data = install.Cleaner(raw_data.data)
-    save = install.Saver(clean_data.cleaned_data)
+    appli = app.Application(tables.Categories)
+    appli.start()
     db.close()
 
-db.connect()
-appli = app.Application(tables.Categories)
-appli.start()
-db.close()
-
+main()
